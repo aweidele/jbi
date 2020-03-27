c8825fe0878767e5a8def92790dcbe3b8647b915
Aaron Weidele <aweidele@r2integrated.com>
2020-03-26 22:05:47 -0400

    laying out the projects

--- single-projects.php
+++ single-projects.php
@@ -14,6 +14,26 @@
       </div>
     </div>
   </header>
+  <article class="project-main">
+    <div>
+      <div class="project-main-inner">
+        <div class="col-1">
+          <?php the_content(); ?>
+          <?php if ( $content['photo_credit'] ) { ?>
+          <p>PHOTO CREDIT: <?php echo $content['photo_credit']; ?></p>
+          <?php } ?>
+        </div>
+        <?php
+          foreach( $content['images'] as $image ) {
+            $w = get_field('grid_width', $image['ID']);
+        ?>
+        <div class="<?php echo $w; ?>">
+          <img src="<?php echo $image['sizes']['proj-hero']?>" alt="<?php echo $image['alt']; ?>">
+        </div>
+        <?php } ?>
+      </div>
+    </div>
+  </article>
 <pre><?php print_r($content); ?></pre>
 <?php
   endwhile;
