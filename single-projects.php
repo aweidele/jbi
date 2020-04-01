9b5ecd177e8aa5629013c106e67c7ef990c9197f
Aaron Weidele <aweidele@r2integrated.com>
2020-04-01 06:11:31 -0400

    Adjust grid on project pages

--- single-projects.php
+++ single-projects.php
@@ -17,6 +17,30 @@
   <article class="project-main">
     <div>
       <div class="project-main-inner">
+        <?php
+          foreach( $content['grid'] as $item ) {
+            $layout = $item['acf_fc_layout'];
+            switch( $layout ) {
+              case 'text_block':
+        ?>
+        <div class="<?php echo $item['width']; ?>">
+          <?php echo $item['text']; ?>
+        </div>
+        <?php
+                break;
+              case 'image':
+        ?>
+        <div class="<?php echo $item['width']; ?>">
+          <picture>
+            <img src="<?php echo $item['image']['sizes']['proj-'.$item['width']]?>" alt="<?php echo $item['image']['alt']; ?>">
+          </picture>
+        </div>
+        <?php
+                break;
+            }
+          }
+        ?>
+        <?php /*
         <div class="col-1">
           <?php the_content(); ?>
           <?php if ( $content['photo_credit'] ) { ?>
@@ -31,6 +55,7 @@
           <img src="<?php echo $image['sizes']['proj-'.$w]?>" alt="<?php echo $image['alt']; ?>">
         </div>
         <?php } ?>
+        */ ?>
       </div>
     </div>
   </article>
