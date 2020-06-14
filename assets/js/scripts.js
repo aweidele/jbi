65149b5b47a5455fa285efbb5ddb389104368862
Aaron Weidele <aweidele@r2integrated.com>
2020-06-14 13:49:47 -0400

    add white background to header on scroll

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -81,3 +81,22 @@ animateIn:!1},e.prototype.swap=function(){if(1===this.core.settings.items&&a.sup
 (function($) {
   
 })(jQuery);
+
+(function($) {
+  if($('body').hasClass('project-detail')) {
+    var heroH = $('.project-hero').height();
+    var headerH = $('.main-header').height();
+    $(window).on('load resize',function() {
+      var heroH = $('.project-hero').height();
+      var headerH = $('.main-header').height();
+    }).on('scroll',function() {
+      var s = $(window).scrollTop() + headerH;
+      console.log(s + '/' + heroH);
+      if(s > heroH) {
+        $('body').addClass('scrolled');
+      } else {
+        $('body').removeClass('scrolled');
+      }
+    });
+  }
+})(jQuery);
---
+++ src/js/project.js
@@ -0,0 +1,18 @@
+(function($) {
+  if($('body').hasClass('project-detail')) {
+    var heroH = $('.project-hero').height();
+    var headerH = $('.main-header').height();
+    $(window).on('load resize',function() {
+      var heroH = $('.project-hero').height();
+      var headerH = $('.main-header').height();
+    }).on('scroll',function() {
+      var s = $(window).scrollTop() + headerH;
+      console.log(s + '/' + heroH);
+      if(s > heroH) {
+        $('body').addClass('scrolled');
+      } else {
+        $('body').removeClass('scrolled');
+      }
+    });
+  }
+})(jQuery);
